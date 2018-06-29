d73e9d4bb0485b29834aecba6105233a8f3e251a
Felipe F. Diogo <felipe.diogo@gmail.com>
2018-06-29 16:08:35 -0300

    nao sei

--- src/main/java/com/felipediogo/hystrix/routes/BookController.java
+++ src/main/java/com/felipediogo/hystrix/routes/BookController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-
 @RestController
 @RequestMapping("data/v1/")
 public class BookController {
